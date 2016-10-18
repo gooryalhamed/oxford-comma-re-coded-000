5ccd5a848f16c76ad9594bd28ad3a6ab75c7a507
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-10-18 20:03:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
-end
+  case array.size
+  when 1
+    return array.join
+  when 2
+    return string = "#{array[0]} and #{array[1]}"
+  when 3
+      return string = "#{array[0]}, #{array[1]}, and #{array[2]}"
+  end
+    if array.size > 3 then
+      index = 3
+      string ="#{array[0]}, #{array[1]}, #{array[2]}, "
+      while index < array.size - 1 do
+        string += "#{array[index]}, "
+        index += 1
+        if index == array.size - 1 then
+         string += "and #{array.last}"
+       end
+      end
+      return string
+    end
+  end
